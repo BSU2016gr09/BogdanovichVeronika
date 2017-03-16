13d7b2fd8ea89e0e2b5def46b051ee7f7bd9e06a
barvenov <barvenovsa@gmail.com>
2017-03-16 21:08:50 +0300

    Плохо

--- 2 сем/4-1.cpp
+++ 2 сем/4-1.cpp
@@ -3,20 +3,20 @@
 #include <cstdlib> 
 #include <cstring> 
 
-#include <time.h> 
+#include <time.h> //Зачем????
 
 using namespace std; 
 
 int main() { 
 char *s1 = "hello hworld"; 
-char s2[2]; 
+char s2[2]; //подумай, как все таки вводить из cin не строку, а именно символ
 int k(0); 
 cout << "Letter: "; 
 cin >> s2; 
 char *s3 = s2; 
 if (strncmp(s1, s3, 1) == 0) 
 k++; 
-for (int i = 1; i <= strlen(s1); i++) { 
+for (int i = 1; i <= strlen(s1); i++) { //Совсем плохо. Фактически ты поступаешь, как в первом семестре. Идёшь по БУКВАМ!!!
 if (*(s1 + i - 1) == ' ' && strncmp(s1 + i, s3, 1) == 0) 
 k++; 
 } 
