a781e3a0bcfc2748a49947e8d8aea3455e85186f
VeronikaBogdanovich <veronicka.angel2012@yandex.by>
2017-04-20 09:31:29 +0200

    Update 4-1.cpp

--- 2 сем/4-1.cpp
+++ 2 сем/4-1.cpp
@@ -1,25 +1,23 @@
 //Определить количество слов в строке, которые начинаются на заданную букву.
-#include <iostream> 
-#include <cstdlib> 
-#include <cstring> 
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
 
-#include <time.h> //Зачем????
+using namespace std;
 
-using namespace std; 
-
-int main() { 
-char *s1 = "hello hworld"; 
-char s2[2]; //подумай, как все таки вводить из cin не строку, а именно символ
-int k(0); 
-cout << "Letter: "; 
-cin >> s2; 
-char *s3 = s2; 
-if (strncmp(s1, s3, 1) == 0) 
-k++; 
-for (int i = 1; i <= strlen(s1); i++) { //Совсем плохо. Фактически ты поступаешь, как в первом семестре. Идёшь по БУКВАМ!!!
-if (*(s1 + i - 1) == ' ' && strncmp(s1 + i, s3, 1) == 0) 
-k++; 
-} 
-cout << k << endl; 
-system("pause"); 
+int main() {
+char *s1 = "hello hworlddas h asfdash h fasd sad fsad h";
+char s2[1]; 
+int k(0);
+cout << "Letter: ";
+cin >> s2[0];
+if (!strncmp(s1, s2, 1)) k++;
+char *s3 = strchr(s1,' ');
+while(s3 != NULL){
+if (!strncmp(s3, s2, 1)) k++;
+s3 = strchr(s3, ' ');
+if (s3 != NULL) s3 += 1;
+}
+cout << k << endl;
+system("pause");
 }
